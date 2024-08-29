49f732f640fbbf20522b9f89a17012eb4c582e0c
Nelson Lamprecht <14348300+NelsonLamprecht@users.noreply.github.com>
2024-08-29 11:24:36 -0500

    adds roles

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
+import { MsalService } from '@azure/msal-angular';
+import { AccountInfo } from '@azure/msal-browser';
 
 type ProfileType = {
   givenName?: string,
   surname?: string,
   userPrincipalName?: string,
-  id?: string
+  id?: string,
+  roles?: string[]
 };
 
 @Component({
@@ -17,19 +20,27 @@ type ProfileType = {
 })
 export class ProfileComponent implements OnInit {
   profile: ProfileType | undefined;
+  account: AccountInfo | null = null;
 
   constructor(
-    private http: HttpClient
-  ) { }
+    private http: HttpClient,
+    private authService: MsalService,
+  ) {
+  }
 
   ngOnInit() {
+    this.account = this.authService.instance.getActiveAccount();
+    this.profile!.roles = this.account?.idTokenClaims?.roles;
     this.getProfile(environment.apiConfig.uri);
   }
 
   getProfile(url: string) {
     this.http.get(url)
-      .subscribe(profile => {
-        this.profile = profile;
+      .subscribe((data: ProfileType) => {
+        this.profile!.givenName = data.givenName;
+        this.profile!.surname = data.surname;
+        this.profile!.userPrincipalName = data.userPrincipalName;
+        this.profile!.id = data.id;
       });
   }
 }
