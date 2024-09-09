57cb9cf37ac4e0deb8578698c589fb3278d64b74
Nelson Lamprecht <14348300+NelsonLamprecht@users.noreply.github.com>
2024-09-09 17:22:14 -0500

    adding prod url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,12 +2,12 @@ export const environment = {
 	production: true,
 	msalConfig: {
 		auth: {
-			clientId: 'ENTER_CLIENT_ID',
-			authority: 'ENTER_AUTHORITY'
+			  clientId: 'd2aeae93-ca05-40b1-9032-7e7842054ad8', //drink redemption
+			  authority: 'https://login.microsoftonline.com/80132c19-eaaa-4b91-acf7-a3ed9a50c97b' // loves prod
 		}
 	},
 	apiConfig: {
-		scopes: ['ENTER_SCOPE'],
-		uri: 'ENTER_URI'
-	}
-};
+	  scopes: ['user.read'],
+	  uri: 'https://graph.microsoft.com/v1.0/me'
+  }
+  };
